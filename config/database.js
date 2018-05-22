0e197bf9fd4c421d719d4e46e1324d12930e3a9f
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-22 14:40:15 +0100

    Add database config with mongoose connection

---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'url' : 'mongodb://localhost:2707/NodeAuth'
+}
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ let app = express();
 let port = process.env.PORT || 8080;
 let mongoose = require('mongoose');
 let bodyParser = require('body-parser');
+let configDB = require('./config/database.js')
 
 app.set("view engine", "ejs")
 
