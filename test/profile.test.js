c5e0b158f392f8b370c27a6152a4550470f30927
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-30 11:47:17 +0100

    Add authentication to profile path. Add test for profile path when user is authenticated and not authenticated

--- test/profile.test.js
+++ test/profile.test.js
@@ -3,25 +3,47 @@ let chaiHttp = require('chai-http')
 let server = require('../server')
 let expect = chai.expect
 let should = chai.should()
+let request = require('supertest')
 process.env.NODE_ENV = 'test';
 
 chai.use(chaiHttp)
 
-describe('Profile Page', function(){
-  it('should load successfully', function(done){
-    chai.request(server)
-    .get('/profile')
+describe('Profile Page Auth', function(){
+  let AuthUser = request.agent(server)
+  let user = {
+    email: 'test1@test.com',
+    password: 'test'
+  }
+
+  before(function(done){
+    AuthUser
+    .post('/login')
+    .send(user)
     .end(function(err, res){
-      res.should.have.status(200)
       done()
     })
   })
 
-  it('should display welcome message', function(done){
+  it('should load successfully', function(done){
+    AuthUser
+    .get('/profile')
+    .end(function(err, res){
+      res.should.have.status(200)
+      res.req.path.should.equal('/profile')
+      res.text.should.contain('profile page')
+      done()
+    })
+  })
+})
+
+describe('Profile Page No Auth', function(){
+  it('redirects to index if not logged in', function(done){
     chai.request(server)
     .get('/profile')
     .end(function(err, res){
-      expect(res.text).to.contain('profile page')
+      res.should.have.status(200)
+      res.req.path.should.equal('/')
+      expect(res.text).to.contain('Login or Register with')
       done()
     })
   })
